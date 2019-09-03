fe667ded475d4d6a2d52ded55429cb18184599fa
Chong.Zhang <woodyzc@gmail.com>
2019-09-03 17:10:23 -0400

    k-mean

--- HW2/my_sol_kmeans.py
+++ HW2/my_sol_kmeans.py
@@ -25,9 +25,44 @@
 #% the kmeans matlab function directly. That is, you need to comment it out.
 
 from sklearn.cluster import KMeans
+import numpy as np
 
 def my_kmeans(image_data, K):
-    kmeans = KMeans(n_clusters=K).fit(image_data)
-    label = kmeans.labels_
-    centroid = kmeans.cluster_centers_
-    return label, centroid
+    
+    # randomly choose K data points as initial centroids
+    centers = image_data[np.random.randint(low = 0, high =image_data.shape[0], size=K),:]
+    
+
+    # create a empty array to store cluster info
+    image_cluster = np.zeros(image_data.shape[0]).astype(int)
+    
+    def update_centers(image_cluster): #update the coordinates of the new centroids
+        for i in range(0,K):
+            new_center = np.mean(image_data[image_cluster==i],axis=0)# get the mean coordinates of the centroids
+            centers[i] = new_center
+        return centers
+    
+    def dist_cal(image_cluster, centers): # calculate the distance between each data point to each center
+                                          # then find the closest center, and store that info in the last column
+        for i in range(image_cluster.shape[0]):
+            dist = np.linalg.norm(image_data[i]-centers,axis=1) #calculate the distance between centroids and each point
+            image_cluster[i] = dist.argmin() # choose the cloest one
+        
+        centers = update_centers(image_cluster)
+        return image_cluster, centers
+    
+    n =0
+    
+    while n<=300: # 300 iteration max
+        centers_old = centers.copy() # hold the cluster info before update
+        
+        image_cluster, centers_new = dist_cal(image_cluster, centers)
+
+        v = centers_new == centers_old # compare the updated and unupdated centroid coordinates
+        if v.all() == False: # if not the same, then continue
+            centers = centers_new
+        else: # if the same, stop
+            n = 301
+            centers = centers_new
+        n += 1
+    return image_cluster, centers
